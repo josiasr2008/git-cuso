4dda8c63791cc942348c074c820512b2b0b78c6e
Josiasr2008 <josiasr2008@gmail.com>
2019-02-07 15:59:51 -0200

    Acrescentou frase bom dia

--- GitTeste/Program.cs
+++ GitTeste/Program.cs
@@ -4,6 +4,8 @@ namespace GitTeste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Bom dia!");
         }
     }
 }
